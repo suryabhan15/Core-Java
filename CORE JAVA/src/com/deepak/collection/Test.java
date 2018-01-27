00d8ec361d605d39b2ed0952a7de1849701560ac
surya <surya@surya-PC>
2018-01-27 13:40:32 +0530

    This is my frist commit by git hub1

---
+++ CORE JAVA/src/com/deepak/collection/Test.java
@@ -0,0 +1,11 @@
+package com.deepak.collection;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("hello this is my frist commit");
+       
+	}
+
+}
