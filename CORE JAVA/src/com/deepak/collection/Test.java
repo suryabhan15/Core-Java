37ff512723de1f3bd50b16e70bc368006224eb9b
surya <surya@surya-PC>
2018-01-27 13:47:16 +0530

    Sceond11

--- CORE JAVA/src/com/deepak/collection/Test.java
+++ CORE JAVA/src/com/deepak/collection/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("hello this is my frist commit");
+       System.out.println("hello this is my frist commit- Sceond commit");
        
 	}
 
